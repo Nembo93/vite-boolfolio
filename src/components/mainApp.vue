0a138fc30e458d6cc8356d85737851375e1694bf
nembo93 <francesco.c193@libero.it>
2023-02-02 16:03:36 +0100

    Ripristina stato precedente

--- src/components/mainApp.vue
+++ src/components/mainApp.vue
@@ -13,6 +13,7 @@ export default {
 
 <template>
   <listProjectApp />
+  <!-- <router-view></router-view> -->
 </template>
 
 <style>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,18 @@
 
 import { createRouter, createWebHistory } from "vue-router";
+// import listProjectApp from "./components/listProjectApp";
 
 const router = createRouter({
 
     history: createWebHistory(),
     routes: [
-        
+        // {
+        //     path: "/",
+        //     name: "listProjectApp",
+        //     component: listProjectApp,
+        //   },
     ]
 
 })
+
+export default router;
